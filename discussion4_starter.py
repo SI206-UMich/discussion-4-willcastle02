e540c6ec230ce6e6f913ec18bdfba3ac7162e30a
Will Castle <willcastle@Wills-MacBook-Air-68.local>
2023-09-20 10:37:13 -0400

    add str/init

--- discussion4_starter.py
+++ discussion4_starter.py
@@ -6,7 +6,9 @@ class Rectangle():
     # It sets an instance variable, "width" to the passed argument, width
     # It sets an instance variable, "height" to the passed argument, height
 
-    # YOUR CODE HERE
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
 
 
 
@@ -15,7 +17,9 @@ class Rectangle():
     # It returns a string, 
     #       "A rectangle with width ____ and height ____"
 
-    # YOUR CODE HERE
+    def __str__(width, height):
+        return f"A rectangle with width {width} and height {height}"
+    
 
 
 
